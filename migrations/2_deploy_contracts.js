77a1cfea9f53a6c4eaed5a6d907e99d3d2240a65
MishaBlaize <67977488+EviLord032@users.noreply.github.com>
2022-07-03 23:16:56 +0300

    feat: add .gitignore and delete build

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const SwapFactory = artifacts.require("SwapFactory");
 
-const adminAddress = "";
-const feeCollectingAddress = "";
+const adminAddress = "0x852Cf8A03c30c50174f091B9438C4914a6dC3A8a";
+const feeCollectingAddress = "0x852Cf8A03c30c50174f091B9438C4914a6dC3A8a";
 
 module.exports = async (deployer) => {
   await deployer.deploy(SwapFactory, adminAddress);
